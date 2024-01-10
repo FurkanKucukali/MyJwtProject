85658088116be6427c52cf982ecb57e93f7a09c8
Furkan <furkankucukali@gmail.com>
2024-01-10 11:41:54 +0300

    add to RegisterUser

---
+++ MyJwtProject/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyJwtProject.Core.Application.Features.Commands;
+
+namespace MyJwtProject.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		private readonly IMediator mediator;
+
+		public AuthController(IMediator mediator)
+		{
+			this.mediator = mediator;
+		}
+
+		[HttpPost("Register")]
+		public async Task<IActionResult> Register (RegisterUserCommandRequest request)
+		{
+			await this.mediator.Send(request);
+			return Created("",request);
+		}
+	}
+}
---
+++ MyJwtProject/Core/Application/Features/Handlers/RegisterUserCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using MyJwtProject.Core.Application.Enums;
+using MyJwtProject.Core.Application.Features.Commands;
+using MyJwtProject.Core.Application.Interfaces;
+using MyJwtProject.Core.Domain;
+
+namespace MyJwtProject.Core.Application.Features.Handlers
+{
+	public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommandRequest>
+	{
+		private readonly IRepository<AppUser> repository;
+
+		public RegisterUserCommandHandler(IRepository<AppUser> repository)
+		{
+			this.repository = repository;
+		}
+
+		public async Task<Unit> Handle(RegisterUserCommandRequest request, CancellationToken cancellationToken)
+		{
+			await repository.CreateAsync(new AppUser
+			{
+				Password = request.Password,
+				Username = request.Username,
+				AppRoleId = (int)RoleType.Member
+			});
+			return Unit.Value;
+		}
+	}
+}
